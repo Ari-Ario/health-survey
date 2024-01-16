c56a049367e4dd718a75b65969a602f8e4b945b7
Ari-Ario <xusrew.z@gmail.com>
2024-01-16 11:49:55 +0100

    homepage responsive

--- www/public/components-form/close.php
+++ www/public/components-form/close.php
@@ -1,5 +1,4 @@
 <section class="close-form" id="close" onclick="closeForm()">
-      <section class="close"><span id="cross">&times;</span></section>
       <button class="close"><span id="cross">&times;</span></button>
 </section>
 <script src="script.js"></script>
--- www/public/components-home/body.php
+++ www/public/components-home/body.php
@@ -6,28 +6,27 @@
     </video>
 </section>
 
-    <section class="content">
+<section class="content">
       <h1>Field The Difference</h1>
       <h2>Market Leader in Global Data Collection for Healthcare Market Research</h2>
       <form  action="./form-one.php" method="POST">
         <input id="myBtn" type="submit" value="Start Survey">
       </form>
     
-        <section class="container-cards" style="display: flex;">
+        <section class="container-cards" >
+          <section class="cards">
+            <h3 class="card-heading">QUANTITATIVE RESEARCH</h3>
+            <h4 class="card-text">Sophisticated techniques designed to engage respondents throughout the duration of the survey, leading to more reliable data.</h4>
+          </section>
         <section class="cards">
-            <section class="card-heading">QUANTITATIVE RESEARCH</section>
-            <section class="card-text">Sophisticated techniques designed to engage respondents throughout the duration of the survey, leading to more reliable data.</section>
-        </section>
-        <section class="cards">
-            <section class="card-heading">TRUSTED SERVICE</section>
-            <section class="card-text">Team of industry experts provide
+            <h3 class="card-heading">TRUSTED SERVICE</h3>
+            <h4 class="card-text">Team of industry experts provide
     24/7 client coverage delivering 
-    reliable results on time</section>
+    reliable results on time</h4>
         </section>
         <section class="cards">
-            <section class="card-heading">USER-CENTRIC DESIGN
-</section>
-            <section class="card-text">Dedicated to crafting an optimal online health survey experience by prioritizing user needs, fostering accessibility, and employing intuitive interfaces.</section>
+            <h3 class="card-heading">USER-CENTRIC DESIGN</h3>
+            <h4 class="card-text">Dedicated to crafting an optimal online health survey experience by prioritizing user needs, fostering accessibility, and employing intuitive interfaces.</h4>
         </section>
     </section>
     <footer>
